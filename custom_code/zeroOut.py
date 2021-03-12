7086cf65707a748e573fabae1519b5bbda6929f8
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2021-03-12 08:43:20 -0600

    zeroOut added

---
+++ custom_code/zeroOut.py
@@ -0,0 +1,5 @@
+from mpf.core.custom_code import CustomCode
+
+  def __init__(self):
+      self.machine.events.post('player_score_reel', value = 0, player_num = 1
+      self.machine.events.post('player_score_reel', value = 0, player_num = 2
