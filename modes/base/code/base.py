48620c3d513296fbd714e33a81585bad607277cd
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2021-05-03 16:57:06 -0500

    ro positioned

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -2,19 +2,18 @@ from mpf.core.mode import Mode
 
 class rollOver(Mode):
 
-    def rollOver_player1(self):
-        if self.machine.game.player1.score >= 100000 and self.machine.game.player1.score < 200000 :
-            self.machine.events.post('rollOver_1_100k')
-        if self.machine.game.player1.score >= 200000 and self.machine.game.player1.score < 300000 :
-            self.machine.events.post('rollOver_1_200k')
-        if self.machine.game.player1.score >= 300000 and self.machine.game.player1.score < 300000 :
-            self.machine.events.post('rollOver_1_300k')
+  def rollOver_player1(self):
+      if self.machine.game.player1.score >= 100000 and self.machine.game.player1.score < 200000 :
+        self.machine.events.post('rollOver_1_100k')
+      if self.machine.game.player1.score >= 200000 and self.machine.game.player1.score < 300000 :
+        self.machine.events.post('rollOver_1_200k')
+      if self.machine.game.player1.score >= 300000 and self.machine.game.player1.score < 300000 :
+        self.machine.events.post('rollOver_1_300k')
 
-    def rollOver_player2(self):
-    def rollOver_player1(self):
-        if self.machine.game.player2.score >= 100000 and self.machine.game.player2.score < 200000 :
-            self.machine.events.post('rollOver_2_100k')
-        if self.machine.game.player2.score >= 200000 and self.machine.game.player2.score < 300000 :
-            self.machine.events.post('rollOver_2_200k')
-        if self.machine.game.player2.score >= 300000 and self.machine.game.player2.score < 300000 :
-            self.machine.events.post('rollOver_2_300k')
+  def rollOver_player2(self):
+      if self.machine.game.player2.score >= 100000 and self.machine.game.player2.score < 200000 :
+        self.machine.events.post('rollOver_2_100k')
+      if self.machine.game.player2.score >= 200000 and self.machine.game.player2.score < 300000 :
+        self.machine.events.post('rollOver_2_200k')
+      if self.machine.game.player2.score >= 300000 and self.machine.game.player2.score < 300000 :
+        self.machine.events.post('rollOver_2_300k')
