7221ca1c0343deb9f2bc577581ab8205c6d0cc36
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2021-05-04 08:25:57 -0500

    detect player score in custom code

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -7,16 +7,18 @@ class Base(Mode):
 
   def mode_start(self, **kwargs):
       self.machine.leds.l_gi11.color('blue')
+      self.add_mode_event_handler('player_score', self.rollOver)
 
-  def rollOver_player1(self, **kwargs):
-    if self.machine.game.player1.score >= 100 and self.machine.game.player1.score < 2000 :
+  def rollOver(self, **kwargs):
+    self.machine.leds.l_gi11.color('yellow')
+
+    if self.machine.game.player1.score >= 300 and self.machine.game.player1.score < 2000 :
         self.machine.events.post('rollOver_1_100k')
     if self.machine.game.player1.score >= 200000 and self.machine.game.player1.score < 300000 :
         self.machine.events.post('rollOver_1_200k')
     if self.machine.game.player1.score >= 300000 and self.machine.game.player1.score < 300000 :
         self.machine.events.post('rollOver_1_300k')
 
-  def rollOver_player2(self):
     if self.machine.game.player2.score >= 100000 and self.machine.game.player2.score < 200000 :
         self.machine.events.post('rollOver_2_100k')
     if self.machine.game.player2.score >= 200000 and self.machine.game.player2.score < 300000 :
