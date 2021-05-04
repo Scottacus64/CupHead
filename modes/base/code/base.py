7b67c4e8c228209023b65cf8cb1c075d4b52128b
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2021-05-04 11:08:32 -0500

    base py working

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -2,26 +2,16 @@ from mpf.core.mode import Mode
 
 class Base(Mode):
 
-  def mode_init(self, machine, name):
+  def mode_init(self):
     self._current_value = None
 
   def mode_start(self, **kwargs):
-      self.machine.leds.l_gi11.color('blue')
+      self.machine.events.post('knockOut')
       self.add_mode_event_handler('player_score', self.rollOver)
 
   def rollOver(self, **kwargs):
-    self.machine.leds.l_gi11.color('yellow')
 
-    if self.machine.game.player1.score >= 300 and self.machine.game.player1.score < 2000 :
-        self.machine.events.post('rollOver_1_100k')
-    if self.machine.game.player1.score >= 200000 and self.machine.game.player1.score < 300000 :
-        self.machine.events.post('rollOver_1_200k')
-    if self.machine.game.player1.score >= 300000 and self.machine.game.player1.score < 300000 :
-        self.machine.events.post('rollOver_1_300k')
 
-    if self.machine.game.player2.score >= 100000 and self.machine.game.player2.score < 200000 :
-        self.machine.events.post('rollOver_2_100k')
-    if self.machine.game.player2.score >= 200000 and self.machine.game.player2.score < 300000 :
-        self.machine.events.post('rollOver_2_200k')
-    if self.machine.game.player2.score >= 300000 and self.machine.game.player2.score < 300000 :
-        self.machine.events.post('rollOver_2_300k')
+    if self.player.score >= 300 and self.player.score < 2000 :
+        if self.player.number == 1:
+            self.machine.events.post('rollOver_1_100k')
