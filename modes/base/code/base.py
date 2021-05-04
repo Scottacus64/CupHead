dbf7c8f2f4ea42ddcf0051c0f2379ef2e0cb901e
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2021-05-03 22:25:19 -0500

    turn gi11 on

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -2,18 +2,24 @@ from mpf.core.mode import Mode
 
 class Base(Mode):
 
-  def rollOver_player1(self):
-      if self.machine.game.player1.score >= 100000 and self.machine.game.player1.score < 200000 :
+  def mode_init(self, machine, name):
+    self._current_value = None
+
+  def mode_start(self, **kwargs):
+      self.machine.leds.l_gi11.color('blue')
+
+  def rollOver_player1(self, **kwargs):
+    if self.machine.game.player1.score >= 100 and self.machine.game.player1.score < 2000 :
         self.machine.events.post('rollOver_1_100k')
-      if self.machine.game.player1.score >= 200000 and self.machine.game.player1.score < 300000 :
+    if self.machine.game.player1.score >= 200000 and self.machine.game.player1.score < 300000 :
         self.machine.events.post('rollOver_1_200k')
-      if self.machine.game.player1.score >= 300000 and self.machine.game.player1.score < 300000 :
+    if self.machine.game.player1.score >= 300000 and self.machine.game.player1.score < 300000 :
         self.machine.events.post('rollOver_1_300k')
 
   def rollOver_player2(self):
-      if self.machine.game.player2.score >= 100000 and self.machine.game.player2.score < 200000 :
+    if self.machine.game.player2.score >= 100000 and self.machine.game.player2.score < 200000 :
         self.machine.events.post('rollOver_2_100k')
-      if self.machine.game.player2.score >= 200000 and self.machine.game.player2.score < 300000 :
+    if self.machine.game.player2.score >= 200000 and self.machine.game.player2.score < 300000 :
         self.machine.events.post('rollOver_2_200k')
-      if self.machine.game.player2.score >= 300000 and self.machine.game.player2.score < 300000 :
+    if self.machine.game.player2.score >= 300000 and self.machine.game.player2.score < 300000 :
         self.machine.events.post('rollOver_2_300k')
